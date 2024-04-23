6a8661ecd6d39b6fd16d89635d7f490cef1370d7
Jeremia Ott <jeremia.ott@edu.fh-joanneum.at>
2024-04-23 13:54:49 +0200

    Add RankingModel code

---
+++ RankingModel/Player.cs
@@ -0,0 +1,55 @@
+﻿namespace RankingModel;
+
+public record class Player(int skill, int emotionalSkill)
+{
+    public int Skill { get; set; } = skill;
+    public int EmotionalSkill { get; set; } = emotionalSkill;
+    public int OffsetSkill { get; set; } = 0;
+
+    public int SkillAfterEmotions
+    {
+        get
+        {
+            var value = Skill + EmotionalSkill;
+            return value > 0 ? value : 0;
+        }
+    }
+
+    public int CurrentElo
+    {
+        get
+        {
+            var value = SkillAfterEmotions + OffsetSkill;
+            return value > 0 ? value : 0;
+        }
+    }
+
+    public string ToRank()
+    {
+        return CurrentElo switch
+        {
+            < 400 => $"Iron: {CurrentElo} Lp",
+            < 800 => $"Bronze: {CurrentElo - 400} Lp",
+            < 1200 => $"Silver: {CurrentElo - 800} Lp",
+            < 1600 => $"Gold: {CurrentElo - 1200} Lp",
+            < 2000 => $"Platin: {CurrentElo - 1600} Lp",
+            < 2400 => $"Emreald: {CurrentElo - 2000} Lp",
+            < 2800 => $"Diamant: {CurrentElo - 2400} Lp",
+            < 3200 => $"Master: {CurrentElo - 2800} Lp",
+            _ => CurrentElo + " Elo",
+        };
+    }
+
+    public void PlayGame()
+    {
+        var random = new Random().Next(0, 1);
+        if (random == 0)
+        {
+            OffsetSkill += 30;
+            EmotionalSkill = 0;
+        }
+
+        OffsetSkill -= 30;
+        EmotionalSkill -= 10;
+    }
+}
--- RankingModel/Program.cs
+++ RankingModel/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using RankingModel;
+
 Console.WriteLine("Hello, World!");
+
+var player = new Player(1300, 0);
+
+for (int i = 0; i < 1000; i++)
+{
+    player.PlayGame();
+}
+
+Console.WriteLine(player);
